279cae24a836325164f72ca57c14b6935ea8c45d
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2025-05-04 07:57:43 +0530

    Done with Navbar and Hero Section

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-20 -left-20 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
+  );
+}
+
+export default App;
